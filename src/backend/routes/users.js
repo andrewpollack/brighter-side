3d6429e00b34575f7f00f7c774e6787b34bda171
andrewpkq <andrewpkq@gmail.com>
2020-09-19 19:30:21 -0700

    Linting done

--- src/backend/routes/users.js
+++ src/backend/routes/users.js
@@ -1,60 +1,51 @@
-const router = require("express").Router();
-let User = require("../models/user.model");
+const router = require('express').Router(); // eslint-disable-line new-cap
+const User = require('../models/user.model');
 
-//User & Password validation dependencies
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+// User & Password validation dependencies
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const logger = require('../utils/logging');
 const validator = require('../utils/validation');
 
 // Code responses
-let GOOD_RESULT = 200;
-let STANDARD_ERROR = 400;
-let NO_RESULT_ERROR = 404;
-let NRF_MESSAGE = "No results found.";
-let ERROR_MESSAGE = "ERROR: ";
-let CURR_ROUTER = "/users";
+const GOOD_RESULT = 200;
+const STANDARD_ERROR = 400;
+const NO_RESULT_ERROR = 404;
+const NRF_MESSAGE = 'No results found.';
+const ERROR_MESSAGE = 'ERROR: ';
+const CURR_ROUTER = '/users';
 
 // Used for debugging
-router.route("/").get((req, res) => {
+router.route('/').get((req, res) => {
+  const endpoint = `GET ${CURR_ROUTER}/`;
+  logger.logEndpoint(endpoint, '');
 
-    function logEndpoint(log_message) {
-        logger.timestampedLog(`GET ${CURR_ROUTER}/ ${log_message}`)
-    }
-
-    logEndpoint("");
-  
-    User.find()
+  User.find()
       .then((users) => res.status(GOOD_RESULT).json(users))
       .catch((err) => res.status(STANDARD_ERROR).json(ERROR_MESSAGE + err));
 });
 
 
 // Used for debugging
-router.route("/:id").get((req, res) => {
+router.route('/:id').get((req, res) => {
+  const endpoint = `GET ${CURR_ROUTER}/${req.params.id}`;
+  logger.logEndpoint(endpoint, 'START');
 
-    function logEndpoint(log_message) {
-        logger.timestampedLog(`GET ${CURR_ROUTER}/${req.params.id} ${log_message}`)
-    }
-  
-    logEndpoint("START");
-  
-    User.findById(req.params.id)
+  User.findById(req.params.id)
       .then((user) => {
-        // No user found
         if (!user) {
-          logEndpoint("COMPLETED: A");
+          // No user found
+          logger.logEndpoint(endpoint, 'COMPLETED: A');
           return res.status(NO_RESULT_ERROR).json(NRF_MESSAGE);
-        }
-        // Found user, returning info
-        else {
-          logEndpoint("COMPLETED: B");
+        } else {
+          // Found user, returning info
+          logger.logEndpoint(endpoint, 'COMPLETED: B');
           return res.status(GOOD_RESULT).json(user);
         }
       })
       .catch((err) => {
-        logEndpoint("FAILED: A");
+        logger.logEndpoint(endpoint, 'FAILED: A');
         return res.status(STANDARD_ERROR).json(ERROR_MESSAGE + err);
       });
 });
@@ -68,41 +59,36 @@ router.route("/:id").get((req, res) => {
  * INPUTS:
  * - token = req.body.token
  */
-router.route("/get/profile").post((req, res) => {
+router.route('/get/profile').post((req, res) => {
+  endpoint = `POST ${CURR_ROUTER}/get/profile`;
+  logger.logEndpoint(endpoint, 'START');
 
-    function logEndpoint(log_message) { 
-        logger.timestampedLog(`POST ${CURR_ROUTER}/get/profile ${log_message}`)
-    }
-  
-    logEndpoint("START")
+  // Token validation
+  const {decoded, errors, isValid} = validator.validateToken(req.body);
 
-    // Token validation
-    const { decoded, errors, isValid } = validator.validateToken(req.body);
-  
-    // Check validation
-    if (!isValid) {
-      logEndpoint("COMPLETED: A");
-      return res.status(STANDARD_ERROR).json(errors);
-    }
+  // Check validation
+  if (!isValid) {
+    logger.logEndpoint(endpoint, 'COMPLETED: A');
+    return res.status(STANDARD_ERROR).json(errors);
+  }
 
-    const user_id = decoded.id;
-    const firstname = decoded.firstname;
-  
-    User.findById(user_id)
+  const userid = decoded.id;
+  // const firstname = decoded.firstname;
+
+  User.findById(userid)
       .then((user) => {
-        // No user found
         if (!user) {
-          logEndpoint("COMPLETED: A");
+          // No user found
+          logger.logEndpoint(endpoint, 'COMPLETED: A');
           return res.status(NO_RESULT_ERROR).json(NRF_MESSAGE);
-        }
-        // Found user, returning info
-        else {
-          logEndpoint("COMPLETED: B");
+        } else {
+          // Found user, returning info
+          logger.logEndpoint(endpoint, 'COMPLETED: B');
           return res.status(GOOD_RESULT).json(user);
         }
       })
       .catch((err) => {
-        logEndpoint("FAILED: A");
+        logger.logEndpoint(endpoint, 'FAILED: A');
         return res.status(STANDARD_ERROR).json(ERROR_MESSAGE + err);
       });
 });
@@ -119,72 +105,68 @@ router.route("/get/profile").post((req, res) => {
  * - firstname = req.body.firstname;
  * - email = req.body.email;
  */
-router.route("/register").post((req, res) => {
+router.route('/register').post((req, res) => {
+  endpoint = `POST ${CURR_ROUTER}/register`;
+  logger.logEndpoint(endpoint, 'START');
 
-    function logEndpoint(log_message) { 
-        logger.timestampedLog(`POST ${CURR_ROUTER}/register ${log_message}`)
-    }
-  
-    logEndpoint("START")
-  
-    // Form validation
-    const { errors, isValid } = validator.validateRegisterInput(req.body);
-  
-    // Check validation
-    if (!isValid) {
-      logEndpoint("COMPLETED: A");
-      return res.status(STANDARD_ERROR).json(errors);
-    }
-  
-    const username = req.body.username;
-    const password = req.body.password;
-    const firstname = req.body.firstname;
-    const email = req.body.email;
-  
-    User.findOne({ email: req.body.email }).then((user) => {
-      if (user) {
-        logEndpoint("COMPLETED: B");
-        return res.status(STANDARD_ERROR).json({ email: "Email already exists" });
-      } 
-      else {
-        const newUser = new User({
-          username,
-          password,
-          firstname,
-          email,
-        });
-  
-        // Salt passwords before saving in database
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser.save()
+  // Form validation
+  const {errors, isValid} = validator.validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    logger.logEndpoint(endpoint, 'COMPLETED: A');
+    return res.status(STANDARD_ERROR).json(errors);
+  }
+
+  const username = req.body.username;
+  const password = req.body.password;
+  const firstname = req.body.firstname;
+  const email = req.body.email;
+
+  User.findOne({email: req.body.email}).then((user) => {
+    if (user) {
+      logger.logEndpoint(endpoint, 'COMPLETED: B');
+      return res.status(STANDARD_ERROR).json({email: 'Email already exists'});
+    } else {
+      const newUser = new User({
+        username,
+        password,
+        firstname,
+        email,
+      });
+
+      // Salt passwords before saving in database
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
               .then((user) => {
                 const payload = {
                   id: user.id,
                   firstname: user.firstname,
                 };
-  
+
                 // Sign token
-                jwt.sign( payload, "secret",
-                  {
-                    expiresIn: 31556926, // 1 year in seconds
-                  },
-                  (err, token) => {
-                    logEndpoint("COMPLETED: C");
-                    return res.status(GOOD_RESULT).json({success: true, token: token, });
-                  }
+                jwt.sign( payload, 'secret',
+                    {
+                      expiresIn: 31556926, // 1 year in seconds
+                    },
+                    (err, token) => {
+                      logger.logEndpoint(endpoint, 'COMPLETED: C');
+                      return res.status(GOOD_RESULT).json(
+                          {success: true, token: token});
+                    },
                 );
               })
-              .catch((err) => { 
-                logEndpoint("FAILED: A");
+              .catch((err) => {
+                logger.logEndpoint(endpoint, 'FAILED: A');
                 return res.status(STANDARD_ERROR).json(ERROR_MESSAGE + err);
               });
-          });
         });
-      }
-    });
+      });
+    }
+  });
 });
 
 
@@ -199,59 +181,57 @@ router.route("/register").post((req, res) => {
  * - username = req.body.username;
  * - password = req.body.password;
  */
-router.route("/login").post((req, res) => {
+router.route('/login').post((req, res) => {
+  endpoint = `POST ${CURR_ROUTER}/login`;
+  logger.logEndpoint(endpoint, 'START');
 
-    function logEndpoint(log_message) { 
-        logger.timestampedLog(`POST ${CURR_ROUTER}/login ${log_message}`)
+  // Form validation
+  const {errors, isValid} = validator.validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    logger.logEndpoint(endpoint, 'COMPLETED: A');
+    return res.status(STANDARD_ERROR).json(errors);
+  }
+
+  const username = req.body.username;
+  const password = req.body.password;
+  // Find user by username
+  User.findOne({username}).then((user) => {
+    // Check if user exists
+    if (!user) {
+      logger.logEndpoint(endpoint, 'COMPLETED: B');
+      return res.status(NO_RESULT_ERROR).json(
+          {usernameNotFound: 'Username not found'});
     }
-  
-    logEndpoint("START")
-  
-    // Form validation
-    const { errors, isValid } = validator.validateLoginInput(req.body);
-  
-    // Check validation
-    if (!isValid) {
-      logEndpoint("COMPLETED: A");
-      return res.status(STANDARD_ERROR).json(errors);
-    }
-  
-    const username = req.body.username;
-    const password = req.body.password;
-    // Find user by username
-    User.findOne({ username }).then((user) => {
-      // Check if user exists
-      if (!user) {
-        logEndpoint("COMPLETED: B");
-        return res.status(NO_RESULT_ERROR).json({ usernameNotFound: "Username not found" });
-      }
-      // Check password
-      bcrypt.compare(password, user.password).then((isMatch) => {
-        if (isMatch) {
-          // User matched
-          // Create JWT Payload
-          const payload = {
-            id: user.id,
-            name: user.firstname,
-          };
-  
-          // Sign token
-          jwt.sign( payload, "secret",
+    // Check password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.firstname,
+        };
+
+        // Sign token
+        jwt.sign( payload, 'secret',
             {
               expiresIn: 31556926, // 1 year in seconds
             },
             (err, token) => {
-              logEndpoint("COMPLETED: C");
-              return res.status(GOOD_RESULT).json({  success: true,  token: token, });
-            }
-          );
-        } 
-        else {
-          logEndpoint("FAILED: A");
-          return res.status(STANDARD_ERROR).json({ passwordIncorrect: "Password incorrect" });
-        }
-      });
+              logger.logEndpoint(endpoint, 'COMPLETED: C');
+              return res.status(GOOD_RESULT).json(
+                  {success: true, token: token});
+            },
+        );
+      } else {
+        logger.logEndpoint(endpoint, 'FAILED: A');
+        return res.status(STANDARD_ERROR).json(
+            {passwordIncorrect: 'Password incorrect'});
+      }
     });
+  });
 });
 
 module.exports = router;
--- src/backend/utils/logging.js
+++ src/backend/utils/logging.js
@@ -1,9 +1,23 @@
-
+/**
+ * Prints out the message with timestamp beforehand
+ *  e.g. [9/19/2020, 6:47:47 PM]: MESSAGE.
+ * @param {String} message
+ */
 function timestampedLog(message) {
-    date_string = new Date().toLocaleString();
-    console.log(`[${date_string}]: ${message}.`);
+  dateString = new Date().toLocaleString();
+  console.log(`[${dateString}]: ${message}.`);
+}
+
+/**
+ * Prints out the endpoint and related message
+ * @param {String} endpoint
+ * @param {String} message
+ */
+function logEndpoint(endpoint, message) {
+  timestampedLog(`${endpoint} ${message}`);
 }
 
 module.exports = {
-    timestampedLog: timestampedLog
+  timestampedLog: timestampedLog,
+  logEndpoint: logEndpoint,
 };

f47e2e0019e618bfbc21685698025f854faff77e
andrewpkq <andrewpkq@gmail.com>
2020-10-16 20:37:14 -0700

    Further cleaning backend

--- src/backend/models/moment.model.js
+++ src/backend/models/moment.model.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// const User = require('./user.model');
 
 const momentSchema = new Schema({
   ownerId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
